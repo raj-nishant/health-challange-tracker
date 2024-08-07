18f2e0ff1790364e697e59a7658184e1cfd658cd
Nishant Raj <79581018+raj-nishant@users.noreply.github.com>
2024-08-08 00:42:07 +0530

    fixed bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import "./App.css";
+// import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Landing from "./components/layout/Landing.js";
 import Auth from "./views/Auth.tsx";
--- src/views/Auth.tsx
+++ src/views/Auth.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
 import LoginForm from "../components/auth/LoginForm";
-import style from "./Auth.module.css";
 import start from "../assets/kid.png";
 import LoginCard from "../components/auth/LoginCard";
 
@@ -11,39 +10,45 @@ const Auth: React.FC = () => {
   if (user) return <Navigate to="/dashboard" />;
 
   return (
-    <div className={style.container}>
-      <section className={style.start}>
-        <img src={start} className={style.mainimage} alt="Start" />
-        <div className={style.startcontent}>
-          <h3>Health Tracking</h3>
-          <p>Your Fitness Journey Starts Here</p>
+    <div className="flex flex-col bg-orange-100 min-h-screen">
+      <section className="flex justify-between w-3/4 mx-auto mt-12">
+        <img
+          src={start}
+          className="w-[350px] h-[380px] animate-fadeInSlideUp"
+          alt="Start"
+        />
+        <div className="flex flex-col justify-center w-3/5 gap-5">
+          <h3 className="text-5xl font-medium text-gray-700 font-poppins animate-fadeInSlideUp">
+            Health Tracking
+          </h3>
+          <p className="text-2xl font-light text-gray-600 font-poppins animate-fadeInSlideUp delay-500">
+            Your Fitness Journey Starts Here
+          </p>
         </div>
       </section>
 
       <LoginForm />
-      <br />
-      <br />
-      <section className={style.challengesoptions}>
-        <p>Features of our application</p>
-        <div className={style.challengeoption}>
+
+      <section className="flex flex-col items-center gap-12 p-8">
+        <p className="text-4xl font-semibold text-gray-700 font-poppins text-center">
+          Features of our application
+        </p>
+        <div className="flex gap-5">
           {[
             {
               id: 1,
               name: "Create Your Own Challenge.",
               desc: "Start by creating a new challenge with a custom title and description. Set your start and end dates, and define the frequency to track your progress effectively.",
-              // imgSrc: running,
             },
             {
               id: 2,
               name: "Set Challenge Frequency",
               desc: "Customize your challenge by defining how often you want to complete it, whether daily or weekly. Stay on track with your personalized schedule.",
-              // imgSrc: diet,
             },
             {
               id: 3,
               name: "Track Completion Status.",
               desc: "Mark each day or week as completed or missed to keep an accurate record of your challenge. Stay motivated by monitoring your consistency.",
-              // imgSrc: book,
             },
             {
               id: 4,
